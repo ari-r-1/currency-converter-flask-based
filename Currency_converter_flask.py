d52d8310a859def62fa0e5bf90f708e0164dd363
Ari R <ariranalyst@gmail.com>
2025-07-04 05:19:12 +0530

    Currency_converter_flask.py

--- Currency_converter_flask.py
+++ Currency_converter_flask.py
@@ -50,12 +50,13 @@ def index():
     return render_template('index.html', result=result, error=error, currencies=supported_currencies)
 
 # Auto-open browser
-def open_browser():
-    webbrowser.open_new("http://127.0.0.1:5000")
+# def open_browser():
+   # webbrowser.open_new("http://127.0.0.1:5000")
 
 if __name__ == '__main__':
-    threading.Timer(1, open_browser).start()
-    app.run(debug=True)
+    # threading.Timer(1, open_browser).start()
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
 
 
 # MIT License
